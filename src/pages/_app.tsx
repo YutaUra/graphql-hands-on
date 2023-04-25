c7bd315b7a4dbea83012317c0a63b96e23ef58c0
YutaUra <yuuta3594@gmail.com>
2023-04-25 18:12:49 +0900

    urql の provider を _app.tsx に追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import {
+  Client,
+  cacheExchange,
+  fetchExchange,
+  Provider as UrqlProvider,
+} from "urql";
+
+const client = new Client({
+  url: "/api/graphql",
+  exchanges: [cacheExchange, fetchExchange],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <UrqlProvider value={client}>
+      <Component {...pageProps} />
+    </UrqlProvider>
+  );
 }
