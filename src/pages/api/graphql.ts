85cf589980f499ef39e7670b7a85e30d444ed78b
YutaUra <yuuta3594@gmail.com>
2023-04-27 12:13:07 +0900

    resolvers に型をつける

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -3,6 +3,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { loadSchemaSync } from "@graphql-tools/load";
 import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
 import { join } from "path";
+import { Resolvers } from "@/resolvers-types";
 
 export const config = {
   api: {
@@ -15,18 +16,20 @@ type NextServerContext = {
   res: NextApiResponse;
 };
 
+const resolvers: Resolvers = {
+  Query: {
+    posts: () => [
+      { id: "post_1", title: "Node.js" },
+      { id: "post_2", title: "React vs Vue" },
+    ],
+  },
+};
+
 const schema = createSchema<NextServerContext>({
   typeDefs: loadSchemaSync(join(process.cwd(), "schema.graphql"), {
     loaders: [new GraphQLFileLoader()],
   }),
-  resolvers: {
-    Query: {
-      posts: () => [
-        { id: "post_1", title: "Node.js" },
-        { id: "post_2", title: "React vs Vue" },
-      ],
-    },
-  },
+  resolvers,
 });
 
 export default createYoga<NextServerContext>({
